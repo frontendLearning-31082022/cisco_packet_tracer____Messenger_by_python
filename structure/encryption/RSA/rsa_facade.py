f2f8e82163132a7ae3bd538c49d65d7df337ecd2
python_user <not_def@mail.rr>
2023-02-17 02:43:49 +0300

    rsa size limit by 100 len(bytes) using chunks class transfering

--- structure/encryption/RSA/rsa_facade.py
+++ structure/encryption/RSA/rsa_facade.py
@@ -1,6 +1,12 @@
+import bz2
+import pickle
+
 from Crypto.PublicKey import RSA as rsa
 from Crypto.Cipher import PKCS1_v1_5 as cipher
 from Crypto import Random
+
+from structure.server_socket import Server_socket
+
 
 class RSA_facade:
     @staticmethod
@@ -10,7 +16,20 @@ class RSA_facade:
 
         key = rsa.importKey(pubKey)
         cip = cipher.new(key)
-        return cip.encrypt(bytess)
+
+        if len(bytess)>100:
+            obj= Rsa_Limit_chunks(bytess,cip)
+            serialized= Server_socket.serialize_object(obj)
+            return serialized
+
+        try:
+            result=cip.encrypt(bytess)
+            return result
+        except ValueError as ept:
+            sad=0
+
+
+        return None
 
     @staticmethod
     def decrypt(bytess, private):
@@ -19,7 +38,21 @@ class RSA_facade:
 
         key = rsa.importKey(private)
         cip = cipher.new(key)
-        result=cip.decrypt(bytess, None)
+
+
+        try:
+            obj = pickle.loads(bytess)
+            if type(obj).__name__ is "Rsa_Limit_chunks":
+                return obj.encrypt_chunks(cip)
+        except Exception as norm:
+        # except pickle.UnpicklingError as norm:
+            ads=0
+
+        try:
+            result=cip.decrypt(bytess, None)
+        except Exception as norm:
+            ads = 0
+
         return result
 
     @staticmethod
@@ -41,3 +74,25 @@ class RSA_facade:
 
         return False
 
+class Rsa_Limit_chunks:
+    def __init__(self,bytes_input,cipher_by_key):
+        n = 100
+        chunks = [bytes_input[i:i + n] for i in range(0, len(bytes_input), n)]
+
+        self.chunks_encrypt=[cipher_by_key.encrypt(i) for i in chunks]
+    def encrypt_chunks(self,cipher_by_key):
+        encrypted=[cipher_by_key.decrypt(i, None) for i in self.chunks_encrypt]
+        return b''.join(encrypted)
+
+
+
+
+
+
+if __name__ == '__main__':
+    pub,priv= RSA_facade.get_private_and_public()
+
+    bbbb=b"adaadsfadsfhfadafhdfsagsdfhsgsdfhghsddfsghsdgsdghdffhdsfhghhdghhfaadsfhfadafhdfsagsdfhsgsdfhghsddfsghsdgsdghdffhdsfhghhdghdafhdfsagsdfhsgsdfhghsddfsghsdgsdghdffhdsfhghhdghdsfhfadafhdfsagsdfhsgsdfhghsddfsghsdgsdghdffhdsfhghhdghsfhfadafhdfsagsdfhsgsdfhghsddfsghsdgsdghdffhadsfhfadafhdfsagsdfhsgsdfhghsddfsghsdgsdghdffhdsfhghhdghdsfhghhdgh"
+    count= len(bbbb)
+    print(count)
+    RSA_facade.encrypt(bbbb,pub)
